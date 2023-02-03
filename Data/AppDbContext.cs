1de9ea21583d1df341c0fff1f7da515101d983af
JayAtSeneca <jpchaudhari@myseneca.ca>
2023-02-03 18:48:27 -0500

    add application db to connect sql server

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SchoolApp.API.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolApp.API.Data
+{
+    public class AppDbContext:IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+    }
+}
