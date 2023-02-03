4f920c7d4cb4ecba758ff404e77183558f9a71c8
JayAtSeneca <jpchaudhari@myseneca.ca>
2023-02-03 18:38:12 -0500

    update home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SchoolApp.API.Data.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,10 +17,18 @@ namespace SchoolApp.API.Controllers
         {
         }
 
-        [HttpGet]
-        public IActionResult Get()
+        [HttpGet("student")]
+        [Authorize(Roles = UserRoles.Student)]
+        public IActionResult GetStudent()
         {
-            return Ok("Welcome to HomeController");
+            return Ok("Welcome to HomeController - Student");
+        }
+
+        [HttpGet("manager")]
+        [Authorize(Roles = UserRoles.Manager)]
+        public IActionResult GetManager()
+        {
+            return Ok("Welcome to HomeController - Manager");
         }
     }
 }
